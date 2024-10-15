9ff33340422cecea03c3b782224a25dbf883e6ed
zhuyongqi9 <zhuyongqi9@gmail.com>
2024-10-15 16:32:57 +0800

    update basic

--- basic_window.cpp
+++ basic_window.cpp
@@ -1,44 +1,31 @@
+#include <algorithm>
 #include <iostream>
-#include <SDL2/SDL.h>
+#include "SDL_wrapper/wrapper.h"
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-
 int main(int argc, char **argv) {
-    SDL_Window* window = NULL;
-
-    //2D image
-    SDL_Surface* screenSurface = NULL;
-
-    //can't call any SDL functions without initializing SDL first
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "Failed to initialize SDL, error: " << SDL_GetError() << std::endl;
-        exit(1);
-    }
-
-    window = SDL_CreateWindow("Basic Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        std::cerr << "Failed to create window, error: " << SDL_GetError() << std::endl;
-        exit(1);
-    }
-
-    screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
-
-    SDL_Event e;
-
-    bool flag = true;
-    while (flag) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                flag = false;
+    try {
+        std::unique_ptr<WWindow> window(new WWindow("Baseic Window", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN));
+        std::unique_ptr<WRenderer> renderer = window->create_renderer(-1, SDL_RENDERER_ACCELERATED);
+        
+        SDL_Event e;
+    
+        bool flag = true;
+        while (flag) {
+            while (SDL_PollEvent(&e)) {
+                if (e.type == SDL_QUIT) {
+                    flag = false;
+                }
             }
+            
+            renderer->set_color(255, 255, 255, 255);
+            renderer->clear();
+            renderer->present();
         }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
 }
