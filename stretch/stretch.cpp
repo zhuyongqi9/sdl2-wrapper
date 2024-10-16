05f76191b9b514b3dac0cfc8c9daaaf273b928a3
zhuyongqi9 <zhuyongqi9@gmail.com>
2024-10-16 15:35:06 +0800

    update stretch

--- stretch/stretch.cpp
+++ stretch/stretch.cpp
@@ -1,100 +1,38 @@
+#include <cstddef>
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <SDL_wrapper/wrapper.h>
 #include <string>
 #include <vector>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-std::string base_dir = "/home/kirin-zhu/Projects/sdl2-snippets/stretch";
-
-SDL_Window* gWindow = NULL;
-
-SDL_Surface* gStretchSurface = NULL;
-SDL_Surface* gScreenSurface = NULL;
-
-bool init() {
-    //can't call any SDL functions without initializing SDL first
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-SDL_Surface* loadSurface(std::string path) {
-    // Load image at specified path 
-    SDL_Surface* loadSurface = SDL_LoadBMP(path.c_str());
-    if (loadSurface == nullptr) {
-        std::cerr << "Unable to laod iamge " << path << " error: " << SDL_GetError() << std::endl;;
-    }
-
-    SDL_Surface* optimizedSurface = nullptr;
-    optimizedSurface = SDL_ConvertSurface(loadSurface, gScreenSurface->format, 0);
-    if (optimizedSurface == nullptr) {
-        std::cerr << "Failed to optimize iamge error: " << SDL_GetError() << std::endl; 
-    } else {
-        SDL_FreeSurface(loadSurface);
-    }
-
-    return optimizedSurface;
-}
-
-void close() {
-    SDL_DestroyWindow(gWindow);
-    SDL_Quit();
-}
-
-bool loadMedia() {
-    bool success = true;
-
-    if ((gStretchSurface = loadSurface(base_dir + "/stretch.bmp")) == NULL) {
-        std::cerr << "Failed to load default image" << std::endl; 
-        success = false;
-    }
-
-    return success;
-}
+const std::string PRO_DIR(MACRO_PROJECT_DIR);
 
 int main(int argc, char **argv) {
-    if (!init()) {
-        std::cerr << "Failed to initialize SDL, error: " << SDL_GetError() << std::endl;
-        exit(0);
-    }
-
-
-    gWindow = SDL_CreateWindow("Basic Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (gWindow == NULL) {
-        std::cerr << "Failed to create window, error: " << SDL_GetError() << std::endl;
-        exit(1);
-    }
-    gScreenSurface = SDL_GetWindowSurface(gWindow);
-
-    if (!loadMedia()) {
-        exit(0);
-    }
-
-    SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(gWindow);
-
-    bool quit = false;
-    SDL_Event e;
-
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }         
+    try {
+        SDL_Initializer sdl_initializer(SDL_INIT_AUDIO);
+        std::unique_ptr<WWindow> window(new WWindow("", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN));
+        std::unique_ptr<WRenderer> renderer(window->create_renderer(-1, SDL_RENDERER_ACCELERATED));
+        std::shared_ptr<WTexture> stretch(renderer->create_texture(WBMPSurface(PRO_DIR + "/stretch/stretch.bmp")));
+        
+        bool quit = false;
+        SDL_Event e;
+    
+        while (!quit) {
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }         
+            }
+            
+            renderer->set_color(255, 255, 255, 255);
+            renderer->clear();
+            stretch->render(nullptr, nullptr);
+            renderer->present();
         }
-
-        SDL_Rect stretchRect;
-        stretchRect.x = 0;
-        stretchRect.y = 0;
-        stretchRect.w = SCREEN_WIDTH;
-        stretchRect.h = SCREEN_HEIGHT;
-
-        SDL_BlitScaled(gStretchSurface, NULL, gScreenSurface, &stretchRect);
-        SDL_UpdateWindowSurface(gWindow);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
     }
     return 0;
 }
