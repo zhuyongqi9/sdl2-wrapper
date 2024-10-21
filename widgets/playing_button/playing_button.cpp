99359c14c2dc06a3ade5e24ca702f8e932a1d4cb
zhuyongqi9 <zhuyongqi9@gmail.com>
2024-10-21 23:47:58 +0800

    add playing button

--- widgets/playing_button/playing_button.cpp
+++ widgets/playing_button/playing_button.cpp
@@ -1,26 +1,26 @@
+
 #include <SDL_wrapper/wrapper.h>
 #include <iostream>
 #include <vector>
 
+const std::string PRO_DIR(MACRO_PROJECT_DIR);
+
 const int SCREEN_WIDTH = 800 * SCALE;
 const int SCREEN_HEIGHT = 600 * SCALE;
 
-const std::string PRO_DIR(MACRO_PROJECT_DIR);
-
-class RecordingButton {
-public: 
-    RecordingButton(WRenderer *renderer, SDL_Point dst): renderer(renderer), dst(dst) {
+class PlayingButton {
+public:
+    PlayingButton(WRenderer *renderer, SDL_Point dst):renderer(renderer),dst(dst) {
         status = stopped;
-        
-        WPNGSurface stopped_icon_surface(WPNGSurface(PRO_DIR + "/widgets/recording_button/recording_button.png"));
-        stopped_icon_surface.set_color_key(0, 0, 0);
-        stopped_icon.reset(renderer->create_texture(&stopped_icon_surface));
-        
-        WPNGSurface recording_icon_surface(WPNGSurface(PRO_DIR + "/widgets/recording_button/recording_button_recording.png"));
-        recording_icon_surface.set_color_key(0, 0, 0);
-        recording_icon.reset(renderer->create_texture(&recording_icon_surface));
-        
         region = { dst.x, dst.y, width, height};
+        
+        WPNGSurface playing_surface(PRO_DIR + "/widgets/playing_button/playing_button_playing.png");
+        playing_surface.set_color_key(0, 0, 0);
+        playing_icon.reset(renderer->create_texture(&playing_surface));
+        
+        WPNGSurface paused_surface(PRO_DIR + "/widgets/playing_button/playing_button_paused.png");
+        paused_surface.set_color_key(0, 0, 0);
+        paused_icon.reset(renderer->create_texture(&paused_surface));
     }
     
     void handle_event(SDL_Event &e) {
@@ -30,38 +30,45 @@ public:
             if (in_region(point, region))  {
                 if (status == stopped) {
                     std::cout << "playing" << std::endl;
-                    status = recording;
-                } else if (status == recording) {
-                    status = stopped;
+                    status = playing;
+                } else if (status == playing) {
+                    std::cout << "paused" << std::endl;
+                    status = paused;
+                } else if (status == paused) {
+                    std::cout << "replaying" << std::endl;
+                    status = playing;
                 }
             }
         } 
-    }
+    };
     
     void render() {
         if (status == stopped) {
-            stopped_icon->render(nullptr, &region);
-        } else if (status == recording) {
-            recording_icon->render(nullptr, &region);
+            playing_icon->render(nullptr, &region);
+        } else if (status == playing) {
+            paused_icon->render(nullptr, &region);
+        } else if (status == paused) {
+            playing_icon->render(nullptr, &region);
         }
     }
 private:
     enum state {
-        recording,
+        stopped,
+        playing,
         paused,
-        stopped
     };
-    WRenderer *renderer; 
-    std::unique_ptr<WTexture> stopped_icon;
-    std::unique_ptr<WTexture> recording_icon;
+    
+    WRenderer *renderer;
+    
     SDL_Point dst;
     SDL_Rect region;
+    int width = 108;
+    int height = 108;
     
     state status;
     
-    //1080 * 658
-    int width = 108;
-    int height = 108;
+    std::unique_ptr<WTexture> playing_icon;
+    std::unique_ptr<WTexture> paused_icon;
     
     bool in_region(SDL_Point &point, SDL_Rect &region) {
         return (point.x >= region.x && point.x <= region.x + region.w)
@@ -74,14 +81,14 @@ int main() {
     SDL_Initializer sdl_initializer(SDL_INIT_AUDIO);
     TTF_Initializer ttf_initializer;
     
-    std::unique_ptr<WWindow> window(new WWindow("Recording Button", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN));
+    std::unique_ptr<WWindow> window(new WWindow("Playing Button", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN));
     std::unique_ptr<WRenderer> renderer(window->create_renderer(-1, SDL_RENDERER_ACCELERATED));
     
     bool quit = false;
     SDL_Event e;
     std::vector<std::string> list_items(0); 
     for (int i = 0; i < 10; i++) list_items.push_back("device " + std::to_string(i));
-    RecordingButton button(renderer.get(), {20, 20});
+    PlayingButton button(renderer.get(), {20, 20});
     
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
